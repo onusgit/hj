052b78668117fac299325203fa01b86c9ce32410
jaysukh <jaysukh.maghodiya@gmail.com>
2016-10-11 08:07:00 +0530

    integrating listing

--- wp-content/themes/classipress/content-ad_listing.php
+++ wp-content/themes/classipress/content-ad_listing.php
@@ -8,41 +8,74 @@
  */
 global $cp_options;
 ?>
+<div class="item col-xs-4 col-lg-4 list-group-item">
+    <div class="thumbnail">
+        <img class="group list-group-image hide" src="http://placehold.it/400x250/000/fff" alt="" />
+        <?php if ($cp_options->ad_images) cp_ad_loop_thumbnail(); ?>
+        <div class="caption">
+            <div class="group inner list-group-item-heading col-md-2">
+                Product title</div>
+            <div class="group inner list-group-item-heading col-md-2">
+                Product title</div>
+            <div class="group inner list-group-item-heading col-md-2">
+                Product title</div>
+            <div class="group inner list-group-item-text col-md-6">
+                <a href="<?php the_permalink(); ?>">
+                    <i class="fa fa-camera-retro font-18 black pull-left"></i>
+                    <?php
+                    if (mb_strlen(get_the_title()) >= 75)
+                        echo mb_substr(get_the_title(), 0, 75) . '...';
+                    else
+                        the_title();
+                    ?>
+                </a>
 
-<div class="post-block-out <?php cp_display_style( 'featured' ); ?>">
+            </div>
+        </div>
+    </div>
+</div>  
+<div class="post-block-out <?php cp_display_style('featured'); ?>">
 
-	<div class="post-block">
+    <div class="post-block">
 
-		<div class="post-left">
+        <div class="post-left">
 
-			<?php if ( $cp_options->ad_images ) cp_ad_loop_thumbnail(); ?>
+            <?php if ($cp_options->ad_images) cp_ad_loop_thumbnail(); ?>
 
-		</div>
+        </div>
 
-		<div class="<?php cp_display_style( array( 'ad_images', 'ad_class' ) ); ?>">
+        <div class="<?php cp_display_style(array('ad_images', 'ad_class')); ?>">
 
-			<?php appthemes_before_post_title(); ?>
+            <?php appthemes_before_post_title(); ?>
 
-			<h3><a href="<?php the_permalink(); ?>"><?php if ( mb_strlen( get_the_title() ) >= 75 ) echo mb_substr( get_the_title(), 0, 75 ) . '...'; else the_title(); ?></a></h3>
+            <h3>
+                <a href="<?php the_permalink(); ?>">
+                    <?php
+                    if (mb_strlen(get_the_title()) >= 75)
+                        echo mb_substr(get_the_title(), 0, 75) . '...';
+                    else
+                        the_title();
+                    ?></a>
+            </h3>
 
-			<div class="clr"></div>
+            <div class="clr"></div>
 
-			<?php appthemes_after_post_title(); ?>
+            <?php appthemes_after_post_title(); ?>
 
-			<div class="clr"></div>
+            <div class="clr"></div>
 
-			<?php appthemes_before_post_content(); ?>
+            <?php appthemes_before_post_content(); ?>
 
-			<p class="post-desc"><?php echo cp_get_content_preview( 160 ); ?></p>
+            <p class="post-desc"><?php echo cp_get_content_preview(160); ?></p>
 
-			<?php appthemes_after_post_content(); ?>
+            <?php appthemes_after_post_content(); ?>
 
-			<div class="clr"></div>
+            <div class="clr"></div>
 
-		</div>
+        </div>
 
-		<div class="clr"></div>
+        <div class="clr"></div>
 
-	</div><!-- /post-block -->
+    </div><!-- /post-block -->
 
 </div><!-- /post-block-out -->
--- wp-content/themes/classipress/home.php
+++ wp-content/themes/classipress/home.php
@@ -57,7 +57,6 @@ $current_user = wp_get_current_user(); // grabs the user info and puts into vars
                     </div>
                 </div>
             </div>
-            <?php echo do_shortcode("[pt_view id=b9aca17dnr]"); ?>
             <div class="grid-list">
                 <div class="container padding-0">
                     <div class="list-grid-icon">
@@ -78,6 +77,22 @@ $current_user = wp_get_current_user(); // grabs the user info and puts into vars
                             <div class="col-md-2 title_nav">City</div>
                             <div class="col-md-6 title_nav">Offer</div>
                         </div>
+                         
+                        <?php
+                        // show all ads but make sure the sticky featured ads don't show up first
+                        $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+                        query_posts(array('post_type' => APP_POST_TYPE, 'ignore_sticky_posts' => 1, 'paged' => $paged));
+                        $total_pages = max(1, absint($wp_query->max_num_pages));
+                        ?>
+
+                        <?php get_template_part('loop', 'ad_listing'); ?>
+
+                        <?php
+                        if ($total_pages > 1) {
+                            ?>
+                            <div class="paging"><a href="<?php echo $post_type_url; ?>"> <?php _e('View More Ads', APP_TD); ?> </a></div>
+                        <?php } ?>
+                            
                         <div class="item col-xs-4 col-lg-4 list-group-item">
                             <div class="thumbnail">
                                 <img class="group list-group-image hide" src="http://placehold.it/400x250/000/fff" alt="" />
